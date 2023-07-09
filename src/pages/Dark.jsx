8b9f2348f82308647bee8de405157f4db77c113a
Persimmon <53931787+DHyeonJ@users.noreply.github.com>
2023-07-10 00:39:50 +0900

    Comment : 설명 추가

--- src/pages/Dark.jsx
+++ src/pages/Dark.jsx
@@ -3,16 +3,20 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Dark() {
+  // useState 초기값을 False로 주고 변경되는 값들을 적용시키기 위해 사용된다.
   const [checked, setChecked] = useState(false);
   const navigate = useNavigate();
 
+  // checkbox 체크여부 변경되었는지 확인한다.
   const handleCheck = () => {
     setChecked(!checked);
   };
   return (
     <>
+      {/* onChange함수를 통해 체크여부를 확인하고 value에 체크여부에 대한 true/false값을 불러온다. */}
       <input type="checkbox" onChange={handleCheck} value={checked} />
       <div>
+        {/* 체크 여부에 따라 화이트/다크모드 구현 */}
         {!checked ? (
           <p>화이트모드!</p>
         ) : (
--- src/pages/Select.jsx
+++ src/pages/Select.jsx
@@ -8,11 +8,13 @@ function Select() {
   // 선택한 값에 따라 값이 변경되어야하니까 useState를 사용함.
   const [Selected, setSelected] = useState("");
   const navigate = useNavigate();
+  // 변경된 값들을 실제로 화면에 보여주기 위해 setSelected에다 입력한 값들을 불러온다.
   const handleSelect = (e) => {
     setSelected(e.target.value);
   };
   return (
     <div>
+      {/* 선택한 값이 value에 들어가고 onChange함수를 통해 값들이 바뀔 수 있도록 한다. */}
       <select onChange={handleSelect} value={Selected}>
         {/* 위에 배열값을 map으로 돌리고 Options를 통해 key는 warning방지 value는 map으로 돌린 값들 가져온다. */}
         {selectList.map((item) => (
